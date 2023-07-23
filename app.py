5adb4a1c59cc92788f87e8070e46dab653738f1d
ananyashukla05 <79189599+ananyashukla05@users.noreply.github.com>
2023-07-24 01:50:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 app = Flask(__name__)
 @app.route('/',methods=["GET","POST"])
 def index():
-    url = "https://www.businesstoday.in/technology/news"
+    url = "https://www.google.com/finance/"
     req = requests.get(url)
     soup = BeautifulSoup(req.content, 'html.parser')
     #outerData = soup.find_all("div",class_="widget-listing",limit=6)
